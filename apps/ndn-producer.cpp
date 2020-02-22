8510093625d1e0a81205c3b835ff95e401646de0
ConorClery <cleryc@tcd.ie>
2020-02-22 19:56:46 +0000

    Renabled regular producer functionality

--- apps/ndn-producer.cpp
+++ apps/ndn-producer.cpp
@@ -93,42 +93,42 @@ Producer::StopApplication()
 void
 Producer::OnInterest(shared_ptr<const Interest> interest)
 {
-  // App::OnInterest(interest); // tracing inside
-  //
-  // NS_LOG_FUNCTION(this << interest);
-  //
-  // if (!m_active)
-  //   return;
-  //
-  // Name dataName(interest->getName());
-  // // dataName.append(m_postfix);
-  // // dataName.appendVersion();
-  //
-  // auto data = make_shared<Data>();
-  // data->setName(dataName);
-  // data->setFreshnessPeriod(::ndn::time::milliseconds(m_freshness.GetMilliSeconds()));
-  //
-  // data->setContent(make_shared< ::ndn::Buffer>(m_virtualPayloadSize));
-  //
-  // Signature signature;
-  // SignatureInfo signatureInfo(static_cast< ::ndn::tlv::SignatureTypeValue>(255));
-  //
-  // if (m_keyLocator.size() > 0) {
-  //   signatureInfo.setKeyLocator(m_keyLocator);
-  // }
-  //
-  // signature.setInfo(signatureInfo);
-  // signature.setValue(::ndn::makeNonNegativeIntegerBlock(::ndn::tlv::SignatureValue, m_signature));
-  //
-  // data->setSignature(signature);
-  //
-  // NS_LOG_INFO("node(" << GetNode()->GetId() << ") responding with Data: " << data->getName());
-  //
-  // // to create real wire encoding
-  // data->wireEncode();
-  //
-  // m_transmittedDatas(data, this, m_face);
-  // m_appLink->onReceiveData(*data);
+  App::OnInterest(interest); // tracing inside
+
+  NS_LOG_FUNCTION(this << interest);
+
+  if (!m_active)
+    return;
+
+  Name dataName(interest->getName());
+  // dataName.append(m_postfix);
+  // dataName.appendVersion();
+
+  auto data = make_shared<Data>();
+  data->setName(dataName);
+  data->setFreshnessPeriod(::ndn::time::milliseconds(m_freshness.GetMilliSeconds()));
+
+  data->setContent(make_shared< ::ndn::Buffer>(m_virtualPayloadSize));
+
+  Signature signature;
+  SignatureInfo signatureInfo(static_cast< ::ndn::tlv::SignatureTypeValue>(255));
+
+  if (m_keyLocator.size() > 0) {
+    signatureInfo.setKeyLocator(m_keyLocator);
+  }
+
+  signature.setInfo(signatureInfo);
+  signature.setValue(::ndn::makeNonNegativeIntegerBlock(::ndn::tlv::SignatureValue, m_signature));
+
+  data->setSignature(signature);
+
+  NS_LOG_INFO("node(" << GetNode()->GetId() << ") responding with Data: " << data->getName());
+
+  // to create real wire encoding
+  data->wireEncode();
+
+  m_transmittedDatas(data, this, m_face);
+  m_appLink->onReceiveData(*data);
 }
 
 
